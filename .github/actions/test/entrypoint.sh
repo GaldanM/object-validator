793903326a18456e320b105ab710a1dc4e7b85a8
Galdan Moulinneuf <14978190+GaldanM@users.noreply.github.com>
2020-05-10 01:22:59 +0200

    Removed yarn install from test job

--- .github/actions/test/entrypoint.sh
+++ .github/actions/test/entrypoint.sh
@@ -1,18 +1,4 @@
 #!/bin/sh -l
 
-# Exit if any subcommand fails
-set -e
-
-if [[ -e node_modules/.bin/jest ]]; then
-    setup=""
-else
-	echo "## Your environment is not ready yet. Installing modules..."
-    if [[ -f yarn.lock ]]; then
-        setup="yarn --non-interactive --silent --ignore-scripts --production=false &&"
-    else
-        setup="NODE_ENV=development yarn install &&"
-    fi
-fi
-
 echo "## Running Jest"
-sh -c "$setup ./node_modules/.bin/jest"
+sh -c "./node_modules/.bin/jest"
